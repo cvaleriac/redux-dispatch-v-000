61d9c4ff7af004496daac35ace92fc0194b8e520
valeria campilongo <cvaleriac@hotmail.it>
2019-10-15 15:53:53 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,18 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
+let state = {count: 0};
+ 
+function changeState(state, action){
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
+  }
+ 
+function dispatch(action){
+  state = changeState(state, action)
+}
 changeState(state, action)
